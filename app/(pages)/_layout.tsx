42fa1f5cfb835125175899ed8370b519bb2133e6
mhsfn00 <mhsfn00@gmail.com>
2025-08-25 05:18:11 -0300

    [Feat] Added dynamic custom header

--- app/(pages)/_layout.tsx
+++ app/(pages)/_layout.tsx
@@ -1,7 +1,26 @@
-import { Stack } from "expo-router";
+import { Stack, useSegments } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const routeTitles: Record<string, string> = {
+    index: "Início",
+    dosage: "Dosagem",
+    intoxication: "Intoxicação"
+}
 
 export default function PagesLayout() {
-    return <Stack
-        screenOptions={{ headerShown: false }}
-    />
+    const segments = useSegments();
+    const routeKey = segments[segments.length -1] || "index";
+    const title = routeTitles[routeKey] || "Início";
+
+    return (
+        <SafeAreaView className="flex-1">
+            <View className="items-center">
+                <Text className="text-2xl p-5">
+                    {title}
+                </Text>
+            </View>
+            <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
+    )
 }
